264a11f5d24e1cd30fc003b0c9ecfaee5e3edba7
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2026-02-14 09:38:03 +0300

    dashboard update

--- BigDataOrdersDashboard/Context/BigDataDbContext.cs
+++ BigDataOrdersDashboard/Context/BigDataDbContext.cs
@@ -15,8 +15,9 @@ namespace BigDataOrdersDashboard.Context
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
---
+++ BigDataOrdersDashboard/ViewComponents/DashboardViewComponents/_DashboardLast5ReviewsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using BigDataOrdersDashboard.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigDataOrdersDashboard.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardLast5ReviewsComponentPartial : ViewComponent
+    {
+        private readonly BigDataDbContext _context;
+        public _DashboardLast5ReviewsComponentPartial(BigDataDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Reviews.OrderByDescending(x => x.ReviewId).Include(y => y.Customer).Include(z => z.Product).Take(5).ToList();
+            return View(values);
+        }
+    }
+}
